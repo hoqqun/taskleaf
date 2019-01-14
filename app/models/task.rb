d8486f08155dc9d073a1d61d34d37891304a6ee7
hoqqun <hoqqun@gmail.com>
2019-01-14 13:23:16 +0900

    end of csv import and export

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  has_one_attached :image
+
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   
@@ -7,10 +9,34 @@ class Task < ApplicationRecord
   belongs_to :user
 
   scope :recent, -> { order(created_at: :desc) }
-end
 
-private
+  def self.csv_attributes
+    attribute_names
+  end
 
-def validate_name_not_including_comma
-  errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      order(:id).each do |task|
+        csv << csv_attributes.map{ |attr| task.send(attr) }
+      end
+    end
+  end
+
+  def self.import(file)
+    binding.pry
+    CSV.foreach(file.path, headers: true) do |row|
+      task = new
+      task.attributes = row.to_hash.slice(*csv_attributes)
+      task.save!
+    end
+  end
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     post :confirm, action: :confirm_new, on: :new
   end
 
-  resources :tasks
+  resources :tasks do
+    post :import, on: :collection
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
