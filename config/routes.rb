fece0759787a136db77afdb64ffc4f69a8f01739
hoqqun <hoqqun@gmail.com>
2019-01-14 14:01:19 +0900

    end of chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,9 @@ Rails.application.routes.draw do
   resources :tasks do
     post :import, on: :collection
   end
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web, at: "/sidekiq"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
